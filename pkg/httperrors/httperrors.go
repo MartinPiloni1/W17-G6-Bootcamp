d370bade0ab7532f0c67af42a198a706461530cc
Martin Piloni <martin.piloni@mercadolibre.com>
2025-06-26 17:34:34 -0300

    feat/add error handling library

---
+++ pkg/httperrors/httperrors.go
@@ -0,0 +1,50 @@
+package httperrors
+
+import "errors"
+
+type NotFoundError struct {
+	Message string
+}
+
+type BadRequestError struct {
+	Message string
+}
+
+type ConflictError struct {
+	Message string
+}
+
+type UnprocessableEntityError struct {
+	Message string
+}
+
+func (e NotFoundError) Error() string {
+	return e.Message
+}
+
+func (e BadRequestError) Error() string {
+	return e.Message
+}
+
+func (e ConflictError) Error() string {
+	return e.Message
+}
+
+func (e UnprocessableEntityError) Error() string {
+	return e.Message
+}
+
+func GetErrorData(err error) (int, string) {
+	switch {
+	case errors.As(err, &BadRequestError{}):
+		return 400, err.Error()
+	case errors.As(err, &NotFoundError{}):
+		return 404, err.Error()
+	case errors.As(err, &ConflictError{}):
+		return 409, err.Error()
+	case errors.As(err, &UnprocessableEntityError{}):
+		return 422, err.Error()
+	default:
+		return 500, "Internal Server Error"
+	}
+}
