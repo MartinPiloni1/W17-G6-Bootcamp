09ef44f6cf431dee9efd788b4e1478bcdbe3ff57
Martin Piloni <martin.piloni@mercadolibre.com>
2025-06-27 11:05:33 -0300

    feat/add json fle loader

---
+++ pkg/utils/file.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"strconv"
+)
+
+func LoadJsonData[T any](filePath string) (map[int]T, error) {
+	dataAsBytes, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var tmpData map[string]T
+	err = json.Unmarshal(dataAsBytes, &tmpData)
+	if err != nil {
+		return nil, err
+	}
+
+	data := make(map[int]T)
+	for k, v := range tmpData {
+		key, err := strconv.Atoi(k)
+		if err != nil {
+			return nil, errors.New("Error while loading Json as data")
+		}
+		data[key] = v
+	}
+
+	return data, nil
+}
