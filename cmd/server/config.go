ce8972144d0be4291aea49b6cc08e75c584e6964
Agustin Adrian Aguero Rivamar <agustin.aguero@mercadolibre.com>
2025-06-27 09:53:40 -0300

    Feat/Routing Proposal

---
+++ cmd/server/config.go
@@ -0,0 +1,61 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+	"gitub.com/aaguero_meli/W17-G6-Bootcamp/internal/application"
+)
+
+type ConfigServerChi struct {
+	ServerAddr            string
+	LoaderFilePathDefault string // here whe can add diverse filepaths to the
+}
+
+type ServerChi struct {
+	ServerAddr     string
+	LoaderFilePath string // here whe can add diverse filepaths to the
+}
+
+func LoadServerConf() (*ServerChi, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Failed to load .env: %s", err)
+	}
+	// default values
+	serverAddr := os.Getenv("ADDRESS")
+
+	// here we can load more files if we use it
+	filePathDefault := os.Getenv("FILE_PATH_DEFAULT")
+
+	if serverAddr == "" {
+		serverAddr = ":8080"
+	}
+
+	// here we should validate if they are setted
+	if filePathDefault == "" {
+		return &ServerChi{}, fmt.Errorf("env variables not setted")
+	}
+
+	return &ServerChi{
+		ServerAddr:     serverAddr,
+		LoaderFilePath: filePathDefault,
+	}, nil
+}
+
+func (a *ServerChi) Run() (err error) {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger) // logger
+
+	healthRouter := application.HealthRouter()
+
+	// mount healthcheck
+	router.Mount("/healthcheck", healthRouter)
+	err = http.ListenAndServe(a.ServerAddr, router)
+	return
+}
--- internal/application/application.go
+++ internal/application/application.go
@@ -1,1 +1,17 @@
 package application
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func HealthRouter() chi.Router {
+	// here we cand add the storage (if any) repository service and handler
+	//mount the endpoints and return to mount them in the Run()
+	router := chi.NewRouter()
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	return router
+}
