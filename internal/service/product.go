9d7a25a34078a7b84060506b0e0d0f81f9e4e8ab
Martin Piloni <martin.piloni@mercadolibre.com>
2025-07-01 12:38:11 -0300

    feat: sort entries before sending response to the get all endpoint

--- internal/service/product.go
+++ internal/service/product.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"slices"
+
 	"github.com/aaguero_meli/W17-G6-Bootcamp/internal/models"
 	"github.com/aaguero_meli/W17-G6-Bootcamp/internal/repository"
+	"github.com/aaguero_meli/W17-G6-Bootcamp/pkg/utils"
 )
 
 type ProductService struct {
@@ -13,20 +16,29 @@ func NewProductService(repo repository.ProductRepositoryInterface) ProductServic
 	return &ProductService{repo: repo}
 }
 
-func (p *ProductService) Create(product models.ProductAtributtes) (models.Product, error) {
+func (p *ProductService) Create(product models.ProductAttributes) (models.Product, error) {
 	return p.repo.Create(product)
 }
 
-func (p *ProductService) GetAll() (map[int]models.Product, error) {
-	return p.repo.GetAll()
+func (p *ProductService) GetAll() ([]models.Product, error) {
+	data, err := p.repo.GetAll()
+	if err != nil {
+		return []models.Product{}, err
+	}
+
+	slicedData := utils.MapToSlice(data)
+	slices.SortFunc(slicedData, func(a, b models.Product) int {
+		return a.ID - b.ID
+	})
+	return slicedData, nil
 }
 
 func (p *ProductService) GetByID(id int) (models.Product, error) {
 	return p.repo.GetByID(id)
 }
 
-func (p *ProductService) Update(id int, productAtributtes models.ProductAtributtes) (models.Product, error) {
-	return p.repo.Update(id, productAtributtes)
+func (p *ProductService) Update(id int, productAttributes models.ProductAttributes) (models.Product, error) {
+	return p.repo.Update(id, productAttributes)
 }
 
 func (p *ProductService) Delete(id int) error {
