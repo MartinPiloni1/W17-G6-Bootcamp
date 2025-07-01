90a1d256538e071ba5e5a45eddc68ed1e0a9c9c3
Martin Piloni <martin.piloni@mercadolibre.com>
2025-07-01 10:22:15 -0300

    remove unnecesary comments

--- internal/service/product.go
+++ internal/service/product.go
@@ -13,27 +13,22 @@ func NewProductService(repo repository.ProductRepositoryInterface) ProductServic
 	return &ProductService{repo: repo}
 }
 
-// Create implements ProductServiceInterface.
 func (p *ProductService) Create(product models.ProductAtributtes) (models.Product, error) {
 	return p.repo.Create(product)
 }
 
-// GetAll implements ProductServiceInterface.
 func (p *ProductService) GetAll() (map[int]models.Product, error) {
 	return p.repo.GetAll()
 }
 
-// GetByID implements ProductServiceInterface.
 func (p *ProductService) GetByID(id int) (models.Product, error) {
 	return p.repo.GetByID(id)
 }
 
-// Update implements ProductServiceInterface.
 func (p *ProductService) Update(id int, productAtributtes models.ProductAtributtes) (models.Product, error) {
 	return p.repo.Update(id, productAtributtes)
 }
 
-// Delete implements ProductServiceInterface.
 func (p *ProductService) Delete(id int) error {
 	return p.repo.Delete(id)
 }
