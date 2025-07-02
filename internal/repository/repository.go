4f7b9f890cd1e72d65db4f0af81a0e818cfc0f82
Laura Mejia <laura.mejismosquera@mercadolibre.com.co>
2025-07-02 10:33:57 -0500

    ajustes pr

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,7 +4,7 @@ import "github.com/aaguero_meli/W17-G6-Bootcamp/pkg/models"
 
 type WarehouseRepository interface {
 	GetAll() (map[int]models.Warehouse, error)
-	Create(Product models.WarehouseAttributes) (models.Warehouse, error)
+	Create(warehouseAtribbutes models.WarehouseAttributes) (models.Warehouse, error)
 	GetByID(id int) (models.Warehouse, error)
 	Update(id int, warehouseAttributes models.WarehouseAttributes) (models.Warehouse, error)
 	Delete(id int) error
