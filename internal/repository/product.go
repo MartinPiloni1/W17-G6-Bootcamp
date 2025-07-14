1406d6fb3916486fd11349a32b60aa942b1b0859
Martin Piloni <martin.piloni@mercadolibre.com>
2025-07-14 10:20:19 -0300

    refactor: Implementation of GetByID product using SQL

--- internal/repository/product.go
+++ internal/repository/product.go
@@ -2,9 +2,11 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 
 	"github.com/aaguero_meli/W17-G6-Bootcamp/internal/models"
+	"github.com/aaguero_meli/W17-G6-Bootcamp/pkg/httperrors"
 )
 
 type ProductRepositoryDB struct {
@@ -22,7 +24,7 @@ func (repository *ProductRepositoryDB) Create(productAttribbutes models.ProductA
 }
 
 func (repository *ProductRepositoryDB) GetAll() ([]models.Product, error) {
-	query := `
+	const query = `
 		SELECT
 			id,
 			description,
@@ -76,7 +78,52 @@ func (repository *ProductRepositoryDB) GetAll() ([]models.Product, error) {
 }
 
 func (repository *ProductRepositoryDB) GetByID(id int) (models.Product, error) {
-	panic("implement")
+	const query = `
+		SELECT
+			id,
+			description,
+			expiration_rate,
+			freezing_rate,
+			height,
+			length,
+			width,
+			netweight,
+			product_code,
+			recommended_freezing_temperature,
+			product_type_id,
+			seller_id
+		FROM products 
+		WHEREE id = ?
+	`
+
+	row := repository.db.QueryRow(query, id)
+	if err := row.Err(); err != nil {
+		return models.Product{}, err
+	}
+
+	var product models.Product
+	err := row.Scan(
+		&product.ID,
+		&product.Description,
+		&product.ExpirationRate,
+		&product.FreezingRate,
+		&product.Height,
+		&product.Length,
+		&product.Width,
+		&product.NetWeight,
+		&product.ProductCode,
+		&product.RecommendedFreezingTemperature,
+		&product.ProductTypeID,
+		&product.SellerID,
+	)
+	if errors.Is(err, sql.ErrNoRows) {
+		return models.Product{},
+			httperrors.NotFoundError{Message: "Product not found"}
+	} else if err != nil {
+		return models.Product{}, fmt.Errorf("database scan error: %w", err)
+	}
+
+	return product, nil
 }
 
 func (repository *ProductRepositoryDB) Update(id int, product models.Product) (models.Product, error) {
