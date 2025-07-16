aa64ebd32b12cf980e2c1105e2aecf5b626c66c1
Laura Mejia <laura.mejismosquera@mercadolibre.com.co>
2025-07-16 09:41:19 -0500

    ajuste pr

--- docs/db/migrations/16_06_2025_carries.sql
+++ docs/db/migrations/16_06_2025_carries.sql
@@ -4,7 +4,8 @@ CREATE TABLE carries (
     `company_name` VARCHAR(100) NOT NULL,
     `address` VARCHAR(100) NOT NULL,
     `telephone` VARCHAR(100) NOT NULL,
-    FOREIGN KEY (locality_id) REFERENCES localities(id)
-        ON UPDATE CASCADE
-        ON DELETE RESTRICT
+    `locality_id` VARCHAR(20) NOT NULL,
+   FOREIGN KEY (locality_id) REFERENCES localities(id)
+       ON UPDATE CASCADE
+       ON DELETE RESTRICT
 );
