965d917891ef0ff333a2a4de21d08c2bb5ec907a
Martin Piloni <martin.piloni@mercadolibre.com>
2025-07-24 12:51:08 -0300

    refactor: update GetAll test to follow table driven tests

--- internal/handler/product_test.go
+++ internal/handler/product_test.go
@@ -1,107 +1,150 @@
 package handler_test
 
 import (
-	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/aaguero_meli/W17-G6-Bootcamp/internal/handler"
-	mocks "github.com/aaguero_meli/W17-G6-Bootcamp/internal/mock/service"
+	mocks "github.com/aaguero_meli/W17-G6-Bootcamp/internal/mocks/service"
 	"github.com/aaguero_meli/W17-G6-Bootcamp/internal/models"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
 // TODO: Move this function to an utils package
 func Ptr[T any](v T) *T { return &v }
 
-func TestProductHandler(t *testing.T) {
-	t.Run("GetAll returns every entry of the DB", func(t *testing.T) {
-		// Arrange
-		ctx := context.Background()
-		products := []models.Product{
-			{
-				ID: 1,
-				ProductAttributes: models.ProductAttributes{
-					Description:                    "Yogurt helado",
-					ExpirationRate:                 7,
-					FreezingRate:                   2,
-					Height:                         10.5,
-					Length:                         20.0,
-					Width:                          15.0,
-					NetWeight:                      1.2,
-					ProductCode:                    "YOG01",
-					RecommendedFreezingTemperature: -5.0,
-					ProductTypeID:                  3,
-					SellerID:                       Ptr(1),
-				},
-			},
-			{
-				ID: 2,
-				ProductAttributes: models.ProductAttributes{
-					Description:                    "Pechuga de pollo",
-					ExpirationRate:                 3,
-					FreezingRate:                   1,
-					Height:                         5.0,
-					Length:                         25.0,
-					Width:                          12.5,
-					NetWeight:                      0.8,
-					ProductCode:                    "POLLO01",
-					RecommendedFreezingTemperature: 0.0,
-					ProductTypeID:                  7,
-					SellerID:                       Ptr(2),
-				},
-			},
-		}
+func TestProductHandler_GetAll(t *testing.T) {
+	// TODO: Generate random products with values in a valid range
+	p1 := models.Product{
+		ID: 1,
+		ProductAttributes: models.ProductAttributes{
+			Description:                    "Yogurt helado",
+			ExpirationRate:                 7,
+			FreezingRate:                   2,
+			Height:                         10.5,
+			Length:                         20.0,
+			Width:                          15.0,
+			NetWeight:                      1.2,
+			ProductCode:                    "YOG01",
+			RecommendedFreezingTemperature: -5.0,
+			ProductTypeID:                  3,
+			SellerID:                       Ptr(1),
+		},
+	}
 
-		serviceMock := &mocks.ProductServiceMock{}
-		serviceMock.On("GetAll", ctx).Return(products, nil)
+	p2 := models.Product{
+		ID: 2,
+		ProductAttributes: models.ProductAttributes{
+			Description:                    "Pechuga de pollo",
+			ExpirationRate:                 3,
+			FreezingRate:                   1,
+			Height:                         5.0,
+			Length:                         25.0,
+			Width:                          12.5,
+			NetWeight:                      0.8,
+			ProductCode:                    "POLLO01",
+			RecommendedFreezingTemperature: 0.0,
+			ProductTypeID:                  7,
+			SellerID:                       Ptr(2),
+		},
+	}
 
-		hd := handler.NewProductHandler(serviceMock)
-		getAllFunc := hd.GetAll()
-		request := httptest.NewRequest(http.MethodGet, "/", nil)
-		response := httptest.NewRecorder()
-
-		expectedCode := http.StatusOK
-		expectedBody := `
+	tests := []struct {
+		testName     string
+		serviceData  []models.Product
+		serviceError error
+		expectedCode int
+		expectedBody string
+	}{
 		{
-			"data": [
+			testName:     "Success: Get all products",
+			serviceData:  []models.Product{p1, p2},
+			serviceError: nil,
+			expectedCode: http.StatusOK,
+			expectedBody: `
+			{
+				"data": [
+					{
+						"id": 1,
+						"description": "Yogurt helado",
+						"expiration_rate": 7,
+						"freezing_rate": 2,
+						"height": 10.5,
+						"length": 20.0,
+						"width": 15.0,
+						"netweight": 1.2,
+						"product_code": "YOG01",
+						"recommended_freezing_temperature": -5.0,
+						"product_type_id": 3,
+						"seller_id": 1
+					},
+					{
+						"id": 2,
+						"description": "Pechuga de pollo",
+						"expiration_rate": 3,
+						"freezing_rate": 1,
+						"height": 5.0,
+						"length": 25.0,
+						"width": 12.5,
+						"netweight": 0.8,
+						"product_code": "POLLO01",
+						"recommended_freezing_temperature": 0.0,
+						"product_type_id": 7,
+						"seller_id": 2
+					}
+				]
+			}`,
+		},
+		{
+			testName:     "Success: Empty DB",
+			serviceData:  []models.Product{},
+			serviceError: nil,
+			expectedCode: http.StatusOK,
+			expectedBody: `
 				{
-					"id": 1,
-					"description": "Yogurt helado",
-					"expiration_rate": 7,
-					"freezing_rate": 2,
-					"height": 10.5,
-					"length": 20.0,
-					"width": 15.0,
-					"netweight": 1.2,
-					"product_code": "YOG01",
-					"recommended_freezing_temperature": -5.0,
-					"product_type_id": 3,
-					"seller_id": 1 
-				},
-				{
-					"id": 2,
-					"description": "Pechuga de pollo",
-					"expiration_rate": 3,
-					"freezing_rate": 1,
-					"height": 5.0,
-					"length": 25.0,
-					"width": 12.5,
-					"netweight": 0.8,
-					"product_code": "POLLO01",
-					"recommended_freezing_temperature": 0.0,
-					"product_type_id": 7,
-					"seller_id": 2  
+					"data": []
 				}
-			]
-		}`
+			`,
+		},
+		{
+			testName:     "Fail: DB Error",
+			serviceData:  nil,
+			serviceError: errors.New("db error"),
+			expectedCode: http.StatusInternalServerError,
+			expectedBody: `
+				{
+					"status": "Internal Server Error",
+					"message": "Internal Server Error"
+				}
+			`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			// Run tests parallel
+			t.Parallel()
 
-		// Act
-		getAllFunc(response, request)
+			// Arrange
+			serviceMock := &mocks.ProductServiceMock{}
+			serviceMock.On("GetAll", mock.Anything).Return(tt.serviceData, tt.serviceError)
 
-		// Assert
-		require.Equal(t, expectedCode, response.Code)
-		require.JSONEq(t, expectedBody, response.Body.String())
-	})
+			hd := handler.NewProductHandler(serviceMock)
+			getAllFunc := hd.GetAll()
+			request := httptest.NewRequest(http.MethodGet, "/", nil)
+			response := httptest.NewRecorder()
+
+			expectedCode := tt.expectedCode
+			expectedBody := tt.expectedBody
+
+			// Act
+			getAllFunc(response, request)
+
+			// Assert
+			require.Equal(t, expectedCode, response.Code)
+			require.JSONEq(t, expectedBody, response.Body.String())
+		})
+	}
 }
