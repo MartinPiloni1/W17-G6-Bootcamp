842fa75ab0a49ecadb72b053e85fbcb493ec8591
Martin Piloni <martin.piloni@mercadolibre.com>
2025-07-25 17:18:12 -0300

    feat: add update service product test

--- internal/service/product_test.go
+++ internal/service/product_test.go
@@ -341,6 +341,156 @@ func TestProductService_GetRecordsPerProduct(t *testing.T) {
 	}
 }
 
+func TestProductService_Update(t *testing.T) {
+	// Arrange
+	updatePayload := models.ProductPatchRequest{
+		Description:                    Ptr(""),
+		ExpirationRate:                 Ptr(1),
+		FreezingRate:                   Ptr(2),
+		Height:                         Ptr(1.0),
+		Length:                         Ptr(1.0),
+		Width:                          Ptr(1.0),
+		NetWeight:                      Ptr(1.0),
+		ProductCode:                    Ptr(""),
+		RecommendedFreezingTemperature: Ptr(1.0),
+		ProductTypeID:                  Ptr(1),
+		SellerID:                       Ptr(1),
+	}
+
+	originalProduct := models.Product{
+		ID: 1,
+		ProductAttributes: models.ProductAttributes{
+			Description:                    "Yogurt helado",
+			ExpirationRate:                 7,
+			FreezingRate:                   2,
+			Height:                         10.5,
+			Length:                         20.0,
+			Width:                          15.0,
+			NetWeight:                      1.2,
+			ProductCode:                    "YOG01",
+			RecommendedFreezingTemperature: -5.0,
+			ProductTypeID:                  3,
+			SellerID:                       Ptr(1),
+		},
+	}
+
+	updatedProduct := models.Product{
+		ID: 1,
+		ProductAttributes: models.ProductAttributes{
+			Description:                    "",
+			ExpirationRate:                 1,
+			FreezingRate:                   2,
+			Height:                         1,
+			Length:                         1,
+			Width:                          1,
+			NetWeight:                      1,
+			ProductCode:                    "",
+			RecommendedFreezingTemperature: 1,
+			ProductTypeID:                  1,
+			SellerID:                       Ptr(1),
+		},
+	}
+
+	tests := []struct {
+		testName             string
+		repositoryGetData    models.Product
+		repositoryGetError   error
+		repositoryPatchData  models.Product
+		repositoryPatchError error
+		idParam              int
+		expectedResp         models.Product
+		expectedError        error
+	}{
+		{
+			testName:             "Success: Should update all fields of a product",
+			repositoryGetData:    originalProduct,
+			repositoryGetError:   nil,
+			repositoryPatchData:  updatedProduct,
+			repositoryPatchError: nil,
+			idParam:              1,
+			expectedResp:         updatedProduct,
+			expectedError:        nil,
+		},
+		{
+			testName:             "Success: Should update a single field of a product",
+			repositoryGetData:    originalProduct,
+			repositoryGetError:   nil,
+			repositoryPatchData:  updatedProduct,
+			repositoryPatchError: nil,
+			idParam:              1,
+			expectedResp:         updatedProduct,
+			expectedError:        nil,
+		},
+		{
+			testName:             "Fail: should return a Conflict Error if a product with the given product_code already exists ",
+			repositoryGetData:    originalProduct,
+			repositoryGetError:   nil,
+			repositoryPatchData:  models.Product{},
+			repositoryPatchError: httperrors.ConflictError{Message: "A product with the given product code already exists"},
+			idParam:              1,
+			expectedResp:         models.Product{},
+			expectedError:        httperrors.ConflictError{Message: "A product with the given product code already exists"},
+		},
+		{
+			testName:             "Fail: should return a Conflict Error if the given seller id doesn't exists",
+			repositoryGetData:    originalProduct,
+			repositoryGetError:   nil,
+			repositoryPatchData:  models.Product{},
+			repositoryPatchError: httperrors.ConflictError{Message: "The given seller id does not exists"},
+			idParam:              1,
+			expectedResp:         models.Product{},
+			expectedError:        httperrors.ConflictError{Message: "The given seller id does not exists"},
+		},
+		{
+			testName:             "Fail: should return a Not Found if a product with the given id doesn't exists",
+			repositoryGetData:    models.Product{},
+			repositoryGetError:   httperrors.NotFoundError{Message: "Product not found"},
+			repositoryPatchData:  models.Product{},
+			repositoryPatchError: nil,
+			idParam:              1,
+			expectedResp:         models.Product{},
+			expectedError:        httperrors.NotFoundError{Message: "Product not found"},
+		},
+		{
+			testName:             "Fail: should return a DB Error",
+			repositoryGetData:    originalProduct,
+			repositoryGetError:   nil,
+			repositoryPatchData:  models.Product{},
+			repositoryPatchError: errors.New("db error"),
+			idParam:              1,
+			expectedResp:         models.Product{},
+			expectedError:        errors.New("db error"),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.testName, func(t *testing.T) {
+			t.Parallel()
+
+			// Arrange
+			repositoryMock := mocks.ProductRepositoryDBMock{}
+			service := service.NewProductServiceDefault(&repositoryMock)
+
+			repositoryMock.
+				On("GetByID", mock.Anything, tc.idParam).
+				Return(tc.repositoryGetData, tc.repositoryGetError)
+
+			if tc.repositoryGetError == nil {
+				repositoryMock.
+					On("Update", mock.Anything, tc.idParam, updatedProduct).
+					Return(tc.repositoryPatchData, tc.repositoryPatchError)
+			}
+
+			// Act
+			result, err := service.Update(context.Background(), tc.idParam, updatePayload)
+
+			// Assert
+			assert.Equal(t, tc.expectedError, err)
+			assert.Equal(t, tc.expectedResp, result)
+		})
+	}
+}
+
 func TestProductService_Delete(t *testing.T) {
 	// Arrange
 	tests := []struct {
